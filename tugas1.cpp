ca87be1cf88bc7829a8fe269aea9eb474b4fdb81
almaudud <almadudiakunngoding@gmail.com>
2026-03-03 22:33:32 +0700

    masukkan selesai setelah mengeluarkan hasil

--- tugas1.cpp
+++ tugas1.cpp
@@ -16,3 +16,17 @@ cin >> l;
 float jumlah(float a, float b, float c){
     return a + b + c; 
 }
+
+int luaspersegip(int a, int b){
+ return a * b;   
+}
+void output (){
+    cout << "hasilnya = " << luaspersegip(p, l) << endl;
+    cout << "terima kasih" << endl;
+}
+
+int main(){//mulai
+    input();
+    output();
+    cout << "hasil penjualan " << jumlah(15.1, 24.9, 67);
+}//selesai
