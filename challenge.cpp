7214b3e39d7464f65f69af4567d9cf428bbfd26e
almaudud <almadudiakunngoding@gmail.com>
2026-03-03 22:08:23 +0700

    menambahkan output dari luas persegi

--- challenge.cpp
+++ challenge.cpp
@@ -2,3 +2,21 @@
 #include <iostream>
 using namespace std;
 
+//deklarasi global
+int p, l, luas;
+
+//implementasi prosedur dan fungsi 
+void input() {
+    cout << "masukkan panjang  = ";
+    cin >> p;
+    cout << "masukkan lebar =  ";
+    cin >> l;
+}
+int luaspersegi(){
+    return p * l;
+}
+
+void output(){
+    cout << "hasilnya = " << luaspersegi() << endl;
+    cout << "terimakasih";
+}
